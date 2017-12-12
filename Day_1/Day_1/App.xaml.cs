e0ad62e1ab0fb71713618eba21fbc142ab53c49c
Jefferson Malicdin <mr.japoron@yahoo.com>
2017-12-12 11:21:21 +0800

    TEst

---
+++ Day_1/Day_1/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace Day_1
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+			//MainPage = new Day_1.MainPage();
+            MainPage = new NavigationPage(new MainPage());
+		}
+
+		protected override void OnStart ()
+		{
+            // Handle when your app starts
+            
+        }
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+
+	}
+}
---
+++ Day_1/Day_1/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Day_1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        //private async void button_OnclickAsync(object sender, EventArgs e)
+        //{
+        //    //Device.BeginInvokeOnMainThread(async () => {
+        //    //    var result = await this.DisplayAlert("Alert!", "Do you really want to continue?", "Yes", "No");
+        //    //    if (result) await Navigation.PushAsync(new Sample()); // or anything else
+        //    //});
+        //    //return true;
+
+        //    var result = await DisplayAlert("title", "Hello", "OK", "Cancel");
+        //    if(result.Equals(true))
+        //    {
+        //        await Navigation.PushAsync(new Sample());
+        //    }
+        //}
+        private async void button_Onclick(object sender, EventArgs e)
+        {
+            //Device.BeginInvokeOnMainThread(async () => {
+            //    var result = await this.DisplayAlert("Alert!", "Do you really want to continue?", "Yes", "No");
+            //    if (result) await Navigation.PushAsync(new Sample()); // or anything else
+            //});
+            //return true;
+
+            var result = await DisplayAlert("title", "Hello", "OK", "Cancel");
+            if (result.Equals(true))
+            {
+                await Navigation.PushAsync(new Sample());
+            }
+        }
+    }
+}
